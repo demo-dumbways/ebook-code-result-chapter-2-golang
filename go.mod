90a44f20092ee09c23c80511482d49f72e131ae6
suryaa6666 <suryaelidanto@gmail.com>
2022-11-03 14:53:55 +0700

    code result

---
+++ go.mod
@@ -0,0 +1,5 @@
+module golang-c2
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	route := mux.NewRouter()
+	// r.GET("/", (req, res)=> { })
+	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	}).Methods("GET")
+
+	fmt.Println("Server running on port 5000")
+	http.ListenAndServe("localhost:5000", route)
+}
