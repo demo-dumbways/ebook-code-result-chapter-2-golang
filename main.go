0a27c8e9e3773ef21d936a02fb1a5ff5d0b5ede0
suryaa6666 <suryaelidanto@gmail.com>
2022-11-03 16:40:39 +0700

    code result

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
-	"text/template"
 
 	"github.com/gorilla/mux"
 )
@@ -17,12 +18,16 @@ var Data = map[string]interface{}{
 func main() {
 	route := mux.NewRouter()
 
+	// static folder
 	route.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
 
+	// routing
 	route.HandleFunc("/", helloWorld).Methods("GET")
-	route.HandleFunc("/home", home).Methods("GET")
+	route.HandleFunc("/home", home).Methods("GET").Name("home")
 	route.HandleFunc("/blog", blogs).Methods("GET")
 	route.HandleFunc("/blog/{id}", blogDetail).Methods("GET")
+	route.HandleFunc("/add-blog", formBlog).Methods("GET")
+	route.HandleFunc("/blog", addBlog).Methods("POST")
 	route.HandleFunc("/contact-me", contactMe).Methods("GET")
 
 	fmt.Println("Server running on port 5000")
@@ -82,6 +87,32 @@ func blogDetail(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	tmpl.Execute(w, resp)
+}
+
+func formBlog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/form-blog.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
+
+func addBlog(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Title : " + r.PostForm.Get("title"))
+	fmt.Println("Content : " + r.PostForm.Get("content"))
+
+	http.Redirect(w, r, "/blog", http.StatusMovedPermanently)
 }
 
 func contactMe(w http.ResponseWriter, r *http.Request) {
