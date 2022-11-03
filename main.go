7376cba1451678aa339fd3f26a2eb5abce540ca4
suryaa6666 <suryaelidanto@gmail.com>
2022-11-03 14:59:18 +0700

    code result

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 
 func main() {
 	route := mux.NewRouter()
-	// r.GET("/", (req, res)=> { })
-	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello World"))
-	}).Methods("GET")
+
+	route.HandleFunc("/", helloWorld).Methods("GET")
 
 	fmt.Println("Server running on port 5000")
 	http.ListenAndServe("localhost:5000", route)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello World!"))
 }
